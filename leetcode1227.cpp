196473303257ec932fe92fe047f3fa5dbe66d09e
skanda99 <skandaupadhyaya1999@gmail.com>
2020-12-22 23:17:41 +0530

    Airplane Seat Assignment Probability

---
+++ leetcode1227.cpp
@@ -0,0 +1,13 @@
+
+// problem: "https://leetcode.com/problems/airplane-seat-assignment-probability/"
+
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+        if(n == 1) {
+            return 1.0;
+        }
+        
+        return 0.5;
+    }
+};
